f0a64c0f3f8a2e65730e6b4208b6bd2e056d1f4d
Ahmad Mahmoud <abuhamdiehahmad@gmail.com>
2024-02-25 20:21:15 +0300

    added Day-3

---
+++ Day-3/React/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Student from "./Student";
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Ahmad says heelo to react</h1>
+            <Student />
+            <Student />
+            <Student />
+        </div>
+    );
+};
+export default App;
---
+++ Day-3/React/my-app/src/Student.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Student = () => {
+    return (
+        <div>
+            <h3>Student component</h3>
+            <ul>
+                <li>Student Name: Ahmad</li>
+                <li>Student Id: 1234</li>
+                <li>Ahmad is not a graduate student</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Student;
